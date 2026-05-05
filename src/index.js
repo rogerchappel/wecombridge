b44084fef2614df2c44054f9e09ed4b5bded5249
Roger Chappel <miscanalysis@gmail.com>
2026-05-06 06:37:35 +1000

    feat: expose public library API

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { WeComBridgeError } from './errors.js';
+export { normalizePayload, readPayloadFile, fixtureName } from './payload.js';
+export { createPolicy, evaluatePolicy } from './policy.js';
+export { createStreamingReply, streamReply } from './stream.js';
+export { inspectFixtures, loadPolicy, renderMarkdown, writeReport } from './inspect.js';
